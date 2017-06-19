74bddfee7b56b367e442485fe339aacf30e2f66c
Sebastian Werner <s.werner@sebastian-software.de>
2017-06-19 14:46:29 +0200

    Added test for class properties

--- __tests__/index.js
+++ __tests__/index.js
@@ -5,10 +5,14 @@ test("ES2015: Classes", () => {
   expect(transform(`class Component{ main() { } }`, options).code).toMatchSnapshot()
 })
 
-test("ESNext: Async/Await", () => {
-  expect(transform(`async function hello() { }`, options).code).toMatchSnapshot()
+test("ES2015: Class Properties: Bound methods", () => {
+  expect(transform(`class Component{ onClick = () => { } }`, options).code).toMatchSnapshot()
 })
 
 test("React: JSX", () => {
   expect(transform(`function render() { return <h1>Hello</h1> }`, options).code).toMatchSnapshot()
+})
+
+test("ESNext: Async/Await", () => {
+  expect(transform(`async function hello() { }`, options).code).toMatchSnapshot()
 })
