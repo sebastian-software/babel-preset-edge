b3b26e0e22d9727966a98c49704ecf72a6d3e5f5
Sebastian Werner <s.werner@sebastian-software.de>
2017-07-04 14:04:37 +0200

    Disabled debug for es2015

--- __tests__/es2015.js
+++ __tests__/es2015.js
@@ -1,7 +1,7 @@
 import buildPreset from "../src"
 import { fixtures, titles, check } from "./core"
 
-const options = buildPreset(null, { modules: false, target: "es2015", sourceMaps: false, debug:true })
+const options = buildPreset(null, { modules: false, target: "es2015", sourceMaps: false })
 
 fixtures.forEach((fileName, index) => {
   test(titles[index], () => check(fileName, options))
