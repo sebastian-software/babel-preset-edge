9d6cc58a79bb2410af353d7534b77a52434b2629
Sebastian Werner <s.werner@sebastian-software.de>
2017-07-04 15:07:19 +0200

    Switched binary over to production rendering

--- __tests__/binary.js
+++ __tests__/binary.js
@@ -1,7 +1,7 @@
 import buildPreset from "../src"
 import { fixtures, titles, check } from "./core"
 
-const options = buildPreset(null, { target: "binary", sourceMaps: false })
+const options = buildPreset(null, { target: "binary", sourceMaps: false, env: "production" })
 
 fixtures.forEach((fileName, index) => {
   test(titles[index], () => check(fileName, options))
