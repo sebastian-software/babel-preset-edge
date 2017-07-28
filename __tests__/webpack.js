bd3ecaa06c34923080294718f0b6efc7e204407f
Sebastian Werner <s.werner@sebastian-software.de>
2017-07-28 15:14:15 +0200

    Added new webpack import specific output test

---
+++ __tests__/webpack.js
@@ -0,0 +1,8 @@
+import buildPreset from "../src"
+import { fixtures, titles, check } from "./core"
+
+const options = buildPreset(null, { modules: false, target: { browsers: "ie 11", node: "8.0.0" }, imports: "webpack", sourceMaps: false })
+
+fixtures.forEach((fileName, index) => {
+  test(titles[index], () => check(fileName, options))
+})
