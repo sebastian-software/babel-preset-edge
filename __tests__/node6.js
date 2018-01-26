8198f1fb58c5f4d6ccfef3c91e7cde2934b9ac81
Sebastian Werner <s.werner@sebastian-software.de>
2018-01-26 19:21:14 +0100

    Added explicit Node v6 test

---
+++ __tests__/node6.js
@@ -0,0 +1,13 @@
+import buildPreset from "../src"
+import { fixtures, titles, check } from "./core"
+
+const options = buildPreset(null, {
+  modules: false,
+  target: "node6",
+  imports: "rollup-nodejs",
+  sourceMaps: false
+})
+
+fixtures.forEach((fileName, index) => {
+  test(titles[index], () => check(fileName, options))
+})
