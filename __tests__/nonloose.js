373f2ea6edced5ae05065894df260a5ebc3866fc
Sebastian Werner <s.werner@sebastian-software.de>
2018-01-26 19:25:03 +0100

    Added non-loose IE11 test

---
+++ __tests__/nonloose.js
@@ -0,0 +1,14 @@
+import buildPreset from "../src"
+import { fixtures, titles, check } from "./core"
+
+const options = buildPreset(null, {
+  modules: false,
+  target: { browsers: "ie 11" },
+  looseMode: false,
+  imports: "rollup-nodejs",
+  sourceMaps: false
+})
+
+fixtures.forEach((fileName, index) => {
+  test(titles[index], () => check(fileName, options))
+})
