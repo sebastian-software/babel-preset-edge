335dd1bbcf617b9acfddd4e69c57b95b10abfb60
Sebastian Werner <s.werner@sebastian-software.de>
2018-04-30 17:39:36 +0200

    Disabled some overrides which are not necessary anymore.

--- __tests__/core.js
+++ __tests__/core.js
@@ -4,10 +4,6 @@ import { readdirSync } from "fs"
 const FIXTURE_ROOT = "./__tests__/__fixtures__/"
 
 export function check(fixture, options) {
-  // Keep formatting for tests (to keep output/diffs readable)
-  options.minified = false
-  options.compact = false
-
   // Ignore local non-configured babelrc
   options.babelrc = false
 
