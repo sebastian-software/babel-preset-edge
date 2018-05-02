5ecd38e296f21ed7765e4f65dc60ca1c8c2a5e3d
Sebastian Werner <s.werner@sebastian-software.de>
2018-05-02 10:53:31 +0200

    Refactored test file names to clarify their content.

--- __tests__/imports-webpack.js
+++ __tests__/imports-webpack.js
@@ -3,7 +3,6 @@ import { fixtures, titles, check } from "./core"
 
 describe("Webpack", () => {
   const options = buildPreset(null, {
-    target: { browsers: "ie 11", node: "8.0.0" },
     imports: "webpack",
     sourceMaps: false
   })
