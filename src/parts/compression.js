799a9eea66011e76b56b53d78c500f17ae50967f
Sebastian Werner <s.werner@sebastian-software.de>
2018-05-09 14:19:34 +0200

    Fine tuning of minification to use presets in all cases. + Cleanups deps

--- src/parts/compression.js
+++ src/parts/compression.js
@@ -1,5 +1,62 @@
 import minifyPreset from "babel-preset-minify"
-import deadCodeEliminationPlugin from "babel-plugin-minify-dead-code-elimination"
+
+const allDisabled = {
+  booleans: false,
+  builtIns: false,
+  consecutiveAdds: false,
+  deadcode: false,
+  evaluate: false,
+  flipComparisons: false,
+  guards: false,
+  infinity: false,
+  mangle: false,
+  memberExpressions: false,
+  mergeVars: false,
+  numericLiterals: false,
+  propertyLiterals: false,
+  regexpConstructors: false,
+  removeUndefined: false,
+  replace: false,
+  simplify: false,
+  simplifyComparisons: false,
+  typeConstructors: false,
+  undefinedToVoid: false
+}
+
+const mediumCompression = {
+  // Based on a black list of too agressive plugins/settings
+
+  // Settings
+  keepFnName: true,
+  keepClassName: true,
+
+  // Disable Agressive Plugins
+  booleans: false,
+  infinity: false,
+  mangle: false,
+  flipComparisons: false,
+  simplify: false
+}
+
+const lowCompression = {
+  // Based on a white list
+  // All disabled + a few things enabled
+  ...allDisabled,
+
+  // Settings
+  keepFnName: true,
+  keepClassName: true,
+
+  // Enable only very basic plugins
+  deadcode: true,
+  guards: true,
+  memberExpressions: true,
+  propertyLiterals: true,
+  regexpConstructors: true,
+  removeUndefined: true,
+  simplifyComparisons: true,
+  typeConstructors: true
+}
 
 export default function compression(presets, plugins, options) {
   // Use basic compression for development/bundling and full compression for production output.
@@ -8,23 +65,16 @@ export default function compression(presets, plugins, options) {
       presets.push(minifyPreset)
 
       if (options.debug) {
-        console.log("- Output: Minified")
+        console.log("- Output: Full Minification")
       }
     } else {
       presets.push([
         minifyPreset,
-        {
-          booleans: false,
-          infinity: false,
-          mangle: false,
-          flipComparisons: false,
-          simplify: false,
-          keepFnName: true
-        }
+        mediumCompression
       ])
 
       if (options.debug) {
-        console.log("- Output: Compressed")
+        console.log("- Output: Medium Compression")
       }
     }
   }
@@ -32,10 +82,13 @@ export default function compression(presets, plugins, options) {
   // Even when no compression is enabled, it makes sense to remove
   // and code paths which are clearly never executed.
   else {
-    plugins.push(deadCodeEliminationPlugin)
+    presets.push([
+      minifyPreset,
+      lowCompression
+    ])
 
     if (options.debug) {
-      console.log("- Output: Cleanup")
+      console.log("- Output: Low Compression")
     }
   }
 }
