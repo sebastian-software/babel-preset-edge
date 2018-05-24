f9e0bbe75d77d490e58afb7899b29d61d6907b17
Sebastian Werner <s.werner@sebastian-software.de>
2018-05-24 21:47:36 +0200

    Added decorators plugin + added support for loose mode to class properties plugin.

--- src/parts/proposals.js
+++ src/parts/proposals.js
@@ -1,17 +1,17 @@
 import classPropertiesPlugin from "@babel/plugin-proposal-class-properties"
 import objectRestSpreadPlugin from "@babel/plugin-proposal-object-rest-spread"
+import decoratorsPlugin from "@babel/plugin-proposal-decorators"
 
 export default function proposals(presets, plugins, options) {
+  // Support for @decorators to wrap other functions/classes
+  // Must be placed before class properties plugin.
+  plugins.push([ decoratorsPlugin, { legacy: options.looseMode }])
+
   // Support for ES7 Class Properties (currently stage-2)
   // class { handleClick = () => { } }
-  plugins.push(classPropertiesPlugin)
+  plugins.push([ classPropertiesPlugin, { loose: true }])
 
   // Support for Object Rest Spread `...` operator in objects.
   // { ...todo, completed: true }
-  plugins.push([
-    objectRestSpreadPlugin,
-    {
-      useBuiltIns: true
-    }
-  ])
+  plugins.push([ objectRestSpreadPlugin, { useBuiltIns: true }])
 }
