28be391a666ee7be0d266ef7f49a523bdf89ac75
Sebastian Werner <s.werner@sebastian-software.de>
2018-05-25 11:06:29 +0200

    Babel macros plugin

--- src/parts/convenience.js
+++ src/parts/convenience.js
@@ -3,6 +3,7 @@ import { resolve as resolvePath } from "path"
 
 import moduleResolver from "babel-plugin-module-resolver"
 import lodashPlugin from "babel-plugin-lodash"
+import macrosPlugin from "babel-plugin-macros"
 
 export default function convenience(presets, plugins, options) {
   // Optimization for cheery-picking from lodash, asyncjs, ramba and recompose.
@@ -10,6 +11,9 @@ export default function convenience(presets, plugins, options) {
   // https://www.npmjs.com/package/babel-plugin-lodash
   // https://github.com/acdlite/recompose#using-babel-lodash-plugin
   plugins.push([ lodashPlugin, { id: options.optimizeModules }])
+
+  // Enables zero-config, importable babel plugins
+  plugins.push(macrosPlugin)
 
   // Supports loading files in source folder without relative folders
   // https://github.com/tleunen/babel-plugin-module-resolver
