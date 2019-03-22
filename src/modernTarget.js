62f6ede7ffc8c7d4dc29fffcfbc436ab3e80c282
Sebastian Werner <s.werner@sebastian-software.de>
2019-03-22 11:11:58 +0100

    Updated modern target browser/nodejs list

--- src/modernTarget.js
+++ src/modernTarget.js
@@ -1,12 +1,11 @@
 export default {
-  node: "8.9.0",
-  electron: "2.0",
+  node: "10.13.0",
   browsers: [
-    "Safari >= 11.1",
-    "iOS >= 11.4",
-    "Edge >= 16",
-    "Chrome >= 66",
-    "ChromeAndroid >= 66",
-    "Firefox >= 60"
+    "Safari >= 12",
+    "iOS >= 12",
+    "Edge >= 17",
+    "Chrome >= 70",
+    "ChromeAndroid >= 70",
+    "Firefox >= 65"
   ]
 }
