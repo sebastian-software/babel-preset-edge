5f75c53d4c95370bf98e63fa411f74ea417839cf
Sebastian Werner <s.werner@sebastian-software.de>
2019-06-04 15:25:48 +0200

    Added Node v12 tests

---
+++ __tests__/transpile-node12.js
@@ -0,0 +1,14 @@
+import buildPreset from "../src"
+import { check, fixtures, titles } from "./core"
+
+describe("Transpile: NodeJS v8", () => {
+  const options = buildPreset(null, {
+    sourceMaps: false,
+    target: "node",
+    transpile: { node: 12 }
+  })
+
+  fixtures.forEach((fileName, index) => {
+    test(titles[index], () => check(fileName, options))
+  })
+})
